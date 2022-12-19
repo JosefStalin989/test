5003f74b47ddbd739b17102417de4cffea9db2cf
Your Name <you@example.com>
2022-12-19 20:51:17 +0100

    fix shit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(expressLayouts)
 app.use(express.static(__dirname + '../public'))
 
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 const db = mongoose.connection
 db.on('error', error => console.error(error))
